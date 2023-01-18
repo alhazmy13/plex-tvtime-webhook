ba068c4e0bf024eeb9faa3e6ccea4048b9496239
alhazmy13 <me@alhazmy13.net>
2023-01-18 22:54:18 +0300

    fix body parser

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,40 @@
 import dayjs from 'dayjs';
 import {config} from "./config";
 import TvTimeApi from "./tvtime/api";
-import bodyParser from "body-parser";
 import express, {Request, Response} from 'express';
+import multer from 'multer';
 
 const app = express();
-const jsonParser = bodyParser.json()
+const bodyMulter = multer();
+
 const port = config.port;
 const host = config.host;
 
-
-app.post('/tvtime', jsonParser,  async (req: Request, res: Response) => {
-
+app.post('/tvtime', bodyMulter.any(),  async (req: Request, res: Response) => {
+  const body = JSON.parse(req.body.payload);
   const plexUser = req.query.user;
   if (!plexUser) {
     return res.status(400).send({'message': 'PlexUserMissing'});
 	}
-  print(`Event : ${req.body.event}`);
-	if (req.body.event != "media.scrobble") {
+  print(`Event : ${body.event}`);
+	if (body.event != "media.scrobble") {
     return res.status(400).send({'message': 'BadEvent'});
 	}
 
-	print(`Account : ${req.body.Account.title}`)
-  if (req.body.Account.title != plexUser) {
+	print(`Account : ${body.Account.title}`)
+  if (body.Account.title != plexUser) {
 		return  res.status(400).send({'message': 'BadUser'});
 	}
 
-  print(`Content type : ${req.body.Metadata.type}`)
-	if (req.body.Metadata.type != "episode") {
+  print(`Content type : ${body.Metadata.type}`)
+	if (body.Metadata.type != "episode") {
     return  res.status(400).send({'message': 'BadType'});
 	}
 
-  const episodeId = parseGuid(req.body.Metadata.Guid);
+  const episodeId = parseGuid(body.Metadata.Guid);
   print(`Episode Id : ${episodeId}`)
 
-  if(await TvTimeApi.markAsWatched(episodeId) === null){
+  if(!await TvTimeApi.markAsWatched(episodeId)){
     print(`Error happend with Episode Id : ${episodeId}`)
     return  res.status(400).send({'message': 'Episode Id : ' + episodeId + ' marked as watched'});    
   }
